0119ee90425ac8f551b577856d0f7f2c9ff6a55b
pyamsoft <developer.pyamsoft@gmail.com>
2026-09-15 07:54:00 -0700

    Robolectric supports API 37

--- main/src/test/java/com/pyamsoft/tetherfi/main/MainViewModelerTest.kt
+++ main/src/test/java/com/pyamsoft/tetherfi/main/MainViewModelerTest.kt
@@ -206,11 +206,6 @@ private fun newViewModeler(
     )
 
 @RunWith(RobolectricTestRunner::class)
-@Config(
-    // Need this here since Robolectric does not yet support API 37 (which is default otherwise)
-    minSdk = Build.VERSION_CODES.O,
-    maxSdk = Build.VERSION_CODES.BAKLAVA,
-)
 class MainViewModelerTest {
 
   @Test
--- tile/src/test/java/com/pyamsoft/tetherfi/tile/ProxyTileViewModelerTest.kt
+++ tile/src/test/java/com/pyamsoft/tetherfi/tile/ProxyTileViewModelerTest.kt
@@ -128,11 +128,6 @@ private fun newViewModeler(
     )
 
 @RunWith(RobolectricTestRunner::class)
-@Config(
-    // Need this here since Robolectric does not yet support API 37 (which is default otherwise)
-    minSdk = Build.VERSION_CODES.O,
-    maxSdk = Build.VERSION_CODES.BAKLAVA,
-)
 class ProxyTileViewModelerTest {
 
   @Test
