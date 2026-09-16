4ed45eadd1e28682c6a81a2a34732d0316086ae7
pyamsoft <developer.pyamsoft@gmail.com>
2026-09-16 07:15:03 -0700

    Prepare for a properly tagged reproducible release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
   defaultConfig {
     applicationId = "com.pyamsoft.tetherfi"
 
-    versionCode = 74
-    versionName = "20260915-SNAPSHOT"
+    versionCode = 75
+    versionName = "20260916-1"
 
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
